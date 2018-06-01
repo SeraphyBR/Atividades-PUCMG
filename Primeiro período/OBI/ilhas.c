883961b7521028015692c56fd0b005c65cbfbf05
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-01 13:33:41 -0300

    Update

--- Primeiro período/OBI/ilhas.c
+++ Primeiro período/OBI/ilhas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main()
-{
+{//Inicio main 
     int N = 0;//Número de ilhas
     int M = 0;//Número de cabos 
     int S = 0;//Ilha do servidor
@@ -18,33 +18,34 @@ int main()
 
     int conexao[M][3];
     
-    for(int i = 0; i < M; i++){
-	do{
-	    scanf("%d", &conexao[i][0]);
-	}while(conexao[i][0] < 1 || conexao[i][0] > N);
+    for(int i = 0; i < M; i++)
+    {//Inicio for 
+	    do{
+	        scanf("%d", &conexao[i][0]);
+	    }while(conexao[i][0] < 1 || conexao[i][0] > N);
 
-	do{
-	    scanf("%d",&conexao[i][1]);
-        }while(conexao[i][1] < 1 || conexao[i][1] > N);
+    	do{
+    	    scanf("%d",&conexao[i][1]);
+            }while(conexao[i][1] < 1 || conexao[i][1] > N);
 
-	do{
-	    scanf("%d",&conexao[i][2]);
-	}while(conexao[i][2] < 1 || conexao[i][2] > 1000);
-    }
+    	do{
+    	    scanf("%d",&conexao[i][2]);
+    	}while(conexao[i][2] < 1 || conexao[i][2] > 1000);
+    }//Fim for  
 
     do{
        scanf("%d", &S);
     }while(S < 1 || S > N);
 	
-    for(int i = 0; i < M; i++){
-	if(conexao[i][0] != S && conexao[i][1] != S){
-
-		if(conexao[i][2] < menor) menor = conexao[i][2];
-		if(conexao[i][2] > maior) maior = conexao[i][2];
-	}
-   
-    }
+    for(int i = 0; i < M; i++)
+    {//Inicio for   
+	    if(conexao[i][0] != S && conexao[i][1] != S)
+        {
+	    	if(conexao[i][2] < menor) menor = conexao[i][2];
+	    	if(conexao[i][2] > maior) maior = conexao[i][2];
+	    }
+    }//Fim for   
     printf("%d\n", maior - menor);
     return(0);
   
-}
+}//Fim main 
