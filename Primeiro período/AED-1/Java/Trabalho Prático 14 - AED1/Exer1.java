605b3a6c0731b9f5dc0236abd590807910101629
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-04 07:26:13 -0300

    Lista 14

---
+++ Primeiro período/AED-1/Java/Trabalho Prático 14 - AED1/Exer1.java
@@ -0,0 +1,24 @@
+//Autor: Luiz Junio <luisjuniorbr@gmail.com>
+//Data:  30-05-2018
+import java.util.Scanner;
+
+public class Exer1
+{//Inicio classe Exer1 
+    public static void main(String[]args)
+    {//Inicio main 
+
+    }//Fim main 
+
+
+
+
+
+
+}//Fim classe Exer1
+
+class Carro
+{//Inicio classe Carro 
+    private String placa;
+    private int ano;
+
+}//Fim classe Carro 
