3454491433cc4e8e0a893a916cc171c009fd97d0
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-10 22:50:20 -0300

    Update

--- Primeiro período/AED-1/Java/Trabalho Prático 15 - AED1/Soma01.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 15 - AED1/Soma01.java
@@ -5,23 +5,34 @@ public class Soma01
 {//Inicio classe Exer01
     public static void main(String[]args)
     {//Inicio main 
-        double num1, num2;
+        double num1 = 0, num2 = 0;
         Scanner ler = new Scanner(System.in);
-    
-        try{//Inicio try
-            System.out.println("O programa calcula a soma de dois Números reais.\n");
-            System.out.print("Digite o primeiro número: ");
-            num1 = ler.nextDouble();
-            System.out.print("Digite o segundo número: ");
-            num2 = ler.nextDouble();
-            System.out.printf("\nA soma eh: %.2f \n",num1 + num2);
-        }//Fim try 
-        catch(InputMismatchException inputMismatchException){
-            System.out.println("Valor inserido não compativel, digite novamente.");
-            main();
-        }
-
-
+        boolean continuaLaco = true;
+        System.out.println("O programa calcula a soma de dois Números reais.\n");
+        do{
+            try{//Inicio try
+                System.out.print("Digite o primeiro número: ");
+                num1 = ler.nextDouble();
+                continuaLaco = false;
+            }//Fim try 
+            catch(InputMismatchException inputMismatchException){
+                System.out.println("Valor inserido não compativel, digite novamente.");
+                ler.nextLine();
+            }
+        }while(continuaLaco);
+        do{
+            try{//Inicio try
+                continuaLaco = true;
+                System.out.print("Digite o segundo número: ");
+                num2 = ler.nextDouble();
+                continuaLaco = false;
+            }//Fim try 
+            catch(InputMismatchException inputMismatchException){
+                System.out.println("Valor inserido não compativel, digite novamente.");
+                ler.nextLine();//Limpar Buffer
+            }
+        }while(continuaLaco);
+        System.out.printf("\nA soma eh: %.2f \n",num1 + num2);
     }//Fim main 
 
 }//Inicio classe Exer01 
