cbc6e367fef5611c10ad1cd55228813979469a1f
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-11 08:01:35 -0300

    Update

--- Primeiro período/AED-1/Java/Trabalho Prático 15 - AED1/Calculadora.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 15 - AED1/Calculadora.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
-import java.lang.close; 
-
+import java.util.InputMismatchException;
 
 public class Calculadora
 {//Inicio Calculadora
     public static void main(String[]args)
-    {//Inicio main 
-        
-        menu();
+    {//Inicio main
+        boolean continuaLaco = true; 
+        do{
+            try{
+            operacoes(menu());
+            }
+            catch(InputMismatchException inputMismatchException){
+                System.out.println("Valor inserido é Inválido!");
+                System.out.println("Faça novamente: \n");
+            } 
+            catch(ArithmeticException arithmeticException){
+                System.out.println("Erro! Divisão por zero!\n");
+            }   
+        }while(continuaLaco);
     }//Fim main 
 
     public static int menu()
     {//Fim menu
         Scanner ler = new Scanner(System.in);
         System.out.print(
-            "\tMenu Calculadora" +
+            "\n\tMenu Calculadora" +
             "\nDigite uma opção seguinte: " +
             "\n0 - Sair" +
             "\t1 - Adição" +
@@ -27,23 +37,37 @@ public class Calculadora
         return op;
     }//Fim menu 
 
+    public static void operacoes(int op)
+    {//Inicio operacoes
+        System.out.print("Digite o primeiro operando: ");
+        double num1 = leReal();
+        System.out.print("Digite o segundo operando: ");
+        double num2 = leReal();
+        switch (op)
+        {//Inicio switch
+            case 0:
+                break;
+            case 1:
+                imprimeSoma(num1,num2);
+                break;
+            case 2:
+                imprimeSubtracao(num1,num2);
+                break;
+            case 3:
+                imprimeMultiplicacao(num1, num2);
+                break;
+            case 4:   
+                imprimeDivisao(num1, num2);  
+                break;
+            default:
+                break;
+        }//Fim switch
+    }//Fim operacoes
 
-    public static double leReal()
+    public static double leReal() throws InputMismatchException
     {//Inicio leReal
-        boolean continuaLaco = true;
         Scanner ler = new Scanner(System.in);
-        double real = 0;
-        do{
-            try{//Inicio try 
-                real = ler.nextDouble();
-                continuaLaco = false;
-            }//Fim try 
-            catch(InputMismatchException inputMismatchException)
-            {
-                System.out.print("Valor inserido não compativel! \nDigite novamente: ");
-                ler.nextLine();
-            }//Fim catch
-        }while(continuaLaco);
+        double real = ler.nextDouble();
         return real;
     }//Fim leReal
 
@@ -51,7 +75,32 @@ public class Calculadora
         return num1 + num2;
     }//Fim soma 
 
+    public static void imprimeSoma(double num1, double num2){
+        System.out.printf("%.2f + %.2f = %.2f\n", num1, num2, soma(num1,num2));
+    }//Fim imprimeSoma 
+
+    public static double subtrai(double num1, double num2){
+        return num1 - num2;
+    }//Fim subtrai
+
+    public static void imprimeSubtracao(double num1, double num2){
+        System.out.printf("%.2f - %.2f = %.2f\n", num1, num2, subtrai(num1,num2));
+    }//Fim imprimeSubtracao
+
+    public static double multiplica(double num1, double num2){
+        return num1 * num2;
+    }//Fim multiplica
+
+    public static void imprimeMultiplicacao(double num1, double num2){
+        System.out.printf("%.2f x %.2f = %.2f\n", num1, num2, multiplica(num1,num2));
+    }//Fim imprimeMultiplicacao 
+
     public static double divide(double num1, double num2) throws ArithmeticException{
+        if(num2 == 0) throw new ArithmeticException();
         return num1 / num2;
-    }
+    }//Fim divide
+
+    public static void imprimeDivisao(double num1, double num2){
+        System.out.printf("%.2f / %.2f = %.2f\n", num1, num2, divide(num1,num2));
+    }//Fim imprimeDivisao
 }//Fim classe Calculadora 
