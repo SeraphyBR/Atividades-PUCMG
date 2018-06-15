b9511f2c6e2cd66a3e3070e846f13c60c68f725e
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-15 08:27:28 -0300

    update

--- Primeiro período/Seminários-1/Linguagem C/Lista 04/quadrado_magico.cpp
+++ Primeiro período/Seminários-1/Linguagem C/Lista 04/quadrado_magico.cpp
@@ -1,16 +1,13 @@
+//Autor: Luiz Junio Veloso Dos Santos
 #include <stdio.h>
-#define true 1
-#define false 0
 
 int main()
 {//Inicio main 
-    int N = 0, i = 0, j = 0;
-    int somaLinha = 0, somaColuna = 0, somaDiagonal = 0;
-    int antigoValor = 0;
-    int repetido;
+    int N = 0, i = 0, j = 0;//Dimensões do quadrado; Variaveis para navegar pelas matrizes/Array;
+    int somaLinha = 0, somaColuna = 0, somaDiagonal = 0;//Soma das linhas, colunas e diagonal;
 
     do{
-        scanf("%d", &N);
+        scanf("%d", &N);//Leitura das Dimensões do quadrado, lado e altura;
     }while(N < 3 || N > 100);
 
     int quadrado[N][N];
@@ -19,23 +16,22 @@ int main()
         for(j = 0; j < N; j++)
         {//Inicio for j
             do{
-                scanf("%d", &quadrado[i][j]);
+                scanf("%d", &quadrado[i][j]);//Leitura dos números do quadrado mágico;
             }while(quadrado[i][j] < 1 || quadrado[i][j] > 100);
-            //if(quadrado[i][j] == antigoValor) repetido = true;
-            //else antigoValor = quadrado[i][j];
+            
         }//Fim for j 
     }//Fim for i  
 
     for(i = 0; i < N; i++){
-        somaLinha += quadrado[i][0];
+        somaLinha += quadrado[i][0];//Somatorio dos valores da primeira linha;
     }
 
     for(j = 0; j < N; j++){
-        somaColuna += quadrado[0][j];
+        somaColuna += quadrado[0][j];//Somatorio dos valores da primeira coluna;
     }
 
     for(i = 0; i < N; i++){
-        somaDiagonal += quadrado[i][i];
+        somaDiagonal += quadrado[i][i];//Somatorio dos valores na Diagonal principal;
     }
 
     if(somaLinha == somaColuna && somaLinha == somaDiagonal) printf("%d\n",somaLinha);
