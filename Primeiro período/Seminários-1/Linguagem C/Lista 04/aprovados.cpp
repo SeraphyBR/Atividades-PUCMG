76b146bd2edd3aec9cde0b88c088580105f5ba13
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-15 07:52:24 -0300

    update

--- Primeiro período/Seminários-1/Linguagem C/Lista 04/aprovados.cpp
+++ Primeiro período/Seminários-1/Linguagem C/Lista 04/aprovados.cpp
@@ -4,23 +4,23 @@
 int main()
 {//Inicio main 
 
-    int alunos = 0;
-    int a = 0, j = 0;
-    int aprovados = 0, reprovados = 0;
-    float sucesso = 0;
+    int alunos = 0;//Número de alunos;
+    int a = 0, j = 0;//Váriaveis para navegar pela matriz/Array;
+    int aprovados = 0, reprovados = 0;//Número de alunos aprovados e reprovados;
+    float sucesso = 0;//Porcentagem de aprovados;
 
     do{
-        scanf("%d", &alunos);
+        scanf("%d", &alunos);//Leitura do número de alunos;
     }while(alunos < 1 || alunos > 100);
 
-    char nome[alunos][10];
+    char nome[alunos][16];//Define um Arranjo de Strings contendo o nome dos alunos, com o máximo de Caracteres;
 
     for(int i = 0; i < alunos; i++){
-        scanf("%s", nome[i]);
+        scanf("%s", nome[i]);//Leitura dos nomes de cada aluno;
     }
 
-    int nota[alunos][4];
-    int notaFinal[alunos];
+    int nota[alunos][4];//Define a matriz contendo as notas de cada aluno, de cada prova;
+    int notaFinal[alunos];//Define um Arranjo contendo a soma das notas de cada prova, de cada aluno;
 
     for(a = 0; a < alunos; a++)
     {//Inicio for a 
@@ -28,9 +28,9 @@ int main()
         for(j = 0; j < 4; j++)
         {//Inicio for j 
             do{
-                scanf("%d", &nota[a][j]);
+                scanf("%d", &nota[a][j]);//Leitura das notas de cada prova;
             }while(nota[a][j] < 0 || nota[a][j] > 25);
-            notaFinal[a] += nota[a][j];
+            notaFinal[a] += nota[a][j];//Somatorio das notas de cada aluno;
         }//Fim for j
     }//Fim for a 
 
@@ -47,10 +47,10 @@ int main()
         }
     }//Fim for 
 
-    printf("%d APROVADOS\n", aprovados);
-    printf("%d REPROVADOS\n", reprovados);
-    sucesso = (float)aprovados/(float)alunos * 100;
-    printf("%.0f%%\n", sucesso);
+    printf("%d APROVADOS\n", aprovados);//Saída do número de aprovados;
+    printf("%d REPROVADOS\n", reprovados);//Saída do número de reprovados;
+    sucesso = (float)aprovados/(float)alunos * 100;//Calculo da Porcentagem de aprovados;
+    printf("%.0f%%\n", sucesso);//Saída da Porcentagem de aprovados;
 
     return(0);
 }//Fim main 
