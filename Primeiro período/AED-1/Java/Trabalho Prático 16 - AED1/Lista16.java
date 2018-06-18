c1791d4e121d1de0f1a7a7a8027ea3a705bbd552
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-17 23:17:10 -0300

    Update

--- Primeiro período/AED-1/Java/Trabalho Prático 16 - AED1/Lista16.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 16 - AED1/Lista16.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
+
 public class Lista16
 {//Inicio classe Lista16
     public static void main(String[]args)
@@ -45,5 +47,51 @@ class Data
         return bissexto;
     }
         
+    public static String mesExtenso(int mes)
+    {//Inicio mesExtenso
+        String extenso = " ";
+        switch(mes)
+        {//Inicio switch 
+            case 1:
+                extenso = "Janeiro";
+                break;
+            case 2:
+                extenso = "Fevereiro";
+                break;
+            case 3:
+                extenso = "Março";
+                break; 
+            case 4:
+                extenso = "Abril";
+                break;  
+            case 5:
+                extenso = "Maio";
+                break; 
+            case 6:
+                extenso = "Junho";
+                break;  
+            case 7:
+                extenso = "Julho";
+                break; 
+            case 8:
+                extenso = "Agosto";
+                break;
+            case 9:
+                extenso = "Setembro";
+                break;
+            case 10:
+                extenso = "Outubro";
+                break;
+            case 11:
+                extenso = "Novembro";
+                break;
+            case 12:
+                extenso = "Dezembro";
+                break; 
+            default:
+                break;    
+        }//Fim switch
+        return extenso; 
+    }//Fim mesExtenso
 
 }//Fim classe Data
