ed6b0edccfb0401488c1beada65c0f7dd6beb6df
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-19 08:33:45 -0300

    Update

--- Primeiro período/AED-1/Java/Trabalho Prático 16 - AED1/Lista16.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 16 - AED1/Lista16.java
@@ -38,8 +38,9 @@ class Data
         this.ano = ano;
     }
 
-    public static boolean ehBissexto(int ano){
+    public boolean ehAnoBissexto(){
         boolean bissexto = false;
+        int ano = this.getAno();
         if ((ano%4 == 0)&&((ano%100 !=0)||(ano%400 == 0))) bissexto = true;
         return bissexto;
     }
@@ -55,5 +56,43 @@ class Data
             return "";
         }     
     }//Fim mesExtenso
+
+    public boolean ehValido()
+    {//Inicio ehValido
+        boolean valido = true;
+        int ano = this.getAno();
+        int mes = this.getMes();
+        int dia = this.getDia();
+        boolean ehAnoBissexto = this.ehAnoBissexto();
+
+        if(ano < 1) valido = false;
+        else if(mes > 12 || mes < 1) valido = false;
+            else if(dia < 1) valido = false;
+                else if(mes == 1 && dia > 31) valido = false;
+                else if(mes == 2 && !ehAnoBissexto && dia > 28) valido = false;
+                else if(mes == 2 && ehAnoBissexto && dia > 29) valido = false;
+                else if(mes == 3 && dia > 31) valido = false;
+                else if(mes == 4 && dia > 30) valido = false;
+                else if(mes == 5 && dia > 31) valido = false;
+                else if(mes == 6 && dia > 30) valido = false;
+                else if(mes == 7 && dia > 31) valido = false;
+                else if(mes == 8 && dia > 31) valido = false;
+                else if(mes == 9 && dia > 30) valido = false;
+                else if(mes == 10 && dia > 31) valido = false;
+                else if(mes == 11 && dia > 30) valido = false;
+                else if(mes == 12 && dia > 31) valido = false;
+        return valido;
+    }//Fim ehValido
+
+    public boolean ehMaisRecente(Data data)
+    {//Inicio ehMaisRecente
+        boolean ehRecente = false;
+        if(this.getAno() >= data.getAno()){
+            if(this.getMes() >= data.getMes()){
+                if(this.getDia() > data.getDia()) ehRecente = true;
+            }
+        }
+        return ehRecente;
+    }//Fim ehMaisRecente
 
 }//Fim classe Data
