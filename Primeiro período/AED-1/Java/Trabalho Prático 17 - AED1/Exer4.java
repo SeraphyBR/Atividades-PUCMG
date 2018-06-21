b2b3595f9942f6b5be20ded29cf8e82f6924d62c
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-21 07:49:59 -0300

    Update

--- Primeiro período/AED-1/Java/Trabalho Prático 17 - AED1/Exer4.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 17 - AED1/Exer4.java
@@ -2,14 +2,22 @@ import java.io.File;
 import java.io.FileNotFoundException; 
 import java.util.Scanner;
 
-public class Exer4{ 
+public class Exer4
+{//Inicio main  
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite o nome do arquivo: ");
+        String nome = ler.nextLine();
+        lerTexto(nome);
+    }
+
     public static void lerTexto(String nomeArq)
     {//Inicio main 
         try{ 
             File arquivo = new File(nomeArq); 
             Scanner scanner = new Scanner(arquivo); 
             while(scanner.hasNext()){ 
-                System.out.print(scanner.nextLine()); 
+                System.out.print(scanner.nextLine() + "\n"); 
             } 
             scanner.close(); 
         }
