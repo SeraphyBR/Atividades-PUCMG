00be4e6bfd86b17a01e120e1fd21d472f67193d3
SeraphyBR <luisjuniorbr@gmail.com>
2018-08-06 07:43:27 -0300

    Update

---
+++ Segundo período/AED-2/Unidade01/Exer02_array.java
@@ -0,0 +1,8 @@
+/*
+    Autor: Luiz Junio <luisjuniorbr@gmail.com>
+    Enunciado: 
+    A) Faca um metodo que receba um array de inteiros e mostre
+    na tela o maior e o menor elementos do array.
+    B) Repita o exercicio acima fazendo menos comparacoes com os
+    elementos do array.
+*/
