5d0a3e73d7ffd5a05b0cf810fd6bb9ff43bb0e7b
SeraphyBR <luisjuniorbr@gmail.com>
2018-08-12 21:35:45 -0300

    Update

---
+++ Segundo período/AED-2/TP01-parte1/Algebra.java
@@ -0,0 +1,84 @@
+/*
+    Autor: Luiz Junio <luisjuniorbr@gmail.com>
+    Enunciado: 
+    Algebra Booleana - Crie um metodo iterativo que recebe uma string contendo uma expressao
+    booleana e o valor de suas entradas e retorna um booleano indicando se a expressao e verdadeira
+    ou falsa. Cada string de entrada e composta por um numero inteiro n indicando o numero de
+    entradas da expressao booleana corrente. Em seguida, a string contem n valores binarios (um
+    para cada entrada) e a expressao booleana. Na saida padrao, para cada linha de entrada, escreva
+    uma linha de saida com SIM / NAO indicando se a expressao corrente e verdadeira ou falsa.
+
+    Exemplo de entrada:
+        2 0 1 and(not(A) , not(B))
+    Exemplo de saida:
+        0
+
+*/
+public class Algebra
+{//Inicio classe Algebra
+    public static void main(String[] args)
+    {//Inicio main 
+        String[] palavra = new String[1000];
+        int numEntrada = 0;
+
+        do{//Leitura da entrada padrao  
+            palavra[numEntrada] = MyIO.readLine();
+        }while (!ehIgual(palavra[numEntrada++],"0"));
+
+        numEntrada--;   //Desconsiderar ultima linha contendo a palavra "0"
+ 
+    }//Fim main 
+
+    public static boolean expressaoBool(String expressao)
+    {//Inicio expressaoBool
+        int[] entrada = new int[3];
+        char operacaoEx;
+        int p = 0;
+
+        int numEntradas = expressao.charAt(0) - 48;
+        for(int i = 0; i < numEntradas; i++)
+        {//Inicio for
+            p = 2 + (2 * i);//Posicao das entradas, considerando espacos
+            entrada[i] = expressao.charAt(p) - 48;
+        }//Fim for
+        p += 2; //Avanca para a operacao principal, mais externa
+        if(expressao.charAt(p) == 'a'){
+            operacaoEx = 'a';//and
+            p += 4;
+        }
+        else if(expressao.charAt(p) == 'o'){
+            operacaoEx = 'o';//or
+            p += 3;
+        }
+        else if(expressao.charAt(p) == 'n'){
+            operacaoEx = 'n';//not
+            p += 4;
+        }
+
+
+    }//Fim expressaoBool
+
+    public static boolean ehIgual(String p1, String p2)
+    {//Inicio ehIgual
+     /***************************************************************
+      * Nome do método: ehIgual
+      * Data da elaboração: 08/08/2018
+      * Data da última alteração: 08/08/2018
+      * Autor: Luiz Junio <luisjuniorbr@gmail.com>
+      * Contexto de ação: Verifica se uma String é igual a outra.
+      * Valor gerado: verdadeiro se forem iguais, falso caso contrário.
+      ****************************************************************
+      */ 
+        boolean igual = true;
+        int i = 0;
+        if(p1.length() != p2.length()) igual = false;
+        else while(i < p1.length() && igual){
+                if(p1.charAt(i) != p2.charAt(i))//Comparacao caractere por caractere
+                    igual = false;
+                i++;
+        }//Fim while
+        return igual;
+
+    }//Fim ehIgual
+
+}//Fim classe Algebra
