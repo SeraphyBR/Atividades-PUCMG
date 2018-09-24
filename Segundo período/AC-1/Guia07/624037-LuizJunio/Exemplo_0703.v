0b30c63e8073e9b509d10c2aecd129253849ae10
SeraphyBR <luisjuniorbr@gmail.com>
2018-09-23 21:09:17 -0300

    update

--- Segundo período/AC-1/Guia07/624037-LuizJunio/Exemplo_0703.v
+++ Segundo período/AC-1/Guia07/624037-LuizJunio/Exemplo_0703.v
@@ -1,5 +1,5 @@
 // ------------------------- 
-// Exemplo_0702 
+// Exemplo_0703
 // Nome: Luiz Junio Veloso Dos Santos 
 // Matricula: 624037
 // ------------------------- 
@@ -17,8 +17,8 @@ module mux (output s, input key, input a, input b);
    wire notKey;
 
    not NOT1 ( notKey, key );
-   and AND1 ( sa, a, key );
-   and AND2 ( sb, b, notKey );
+   and AND1 ( sa, a, notKey );
+   and AND2 ( sb, b, key );
 
    or OR1 (s, sa, sb);
 endmodule //mux
@@ -28,15 +28,18 @@ endmodule //mux
 //  Selecionavel (paralelas, 1 resposta)
 //  1-bit
 // ------------------------- 
-module lu (output s, input key, input a, input b); 
+module lu (output sa, output sb, input key, input a, input b); 
     
-    wire s0;
-    wire s1;
+    wire s0, s1, s2, s3;
 
     // descrever por portas 
-    and AND1 ( s0, a, b ); 
-    or  OR1  ( s1, a, b );
-    mux MUX1 (s, key, s0, s1);
+    and AND1   ( s0, a, b );
+    nand NAND1 ( s1, a, b );
+    or  OR1    ( s2, a, b );
+    nor NOR1   ( s3, a, b );
+
+    mux MUX1 (sa, key, s0, s1);
+    mux MUX2 (sb, key, s2, s3);
 
 endmodule // mux 
  
@@ -46,19 +49,20 @@ module main;
        reg  x; 
        reg  y;
        reg key;
-       output s;
+       wire s0;
+       wire s1;
  
-       lu LU1 (s, key, x, y); 
+       lu LU1 (s0, s1, key, x, y); 
  
 // ------------------------- parte principal 
  
    initial 
    begin : main 
-        $display("Exemplo_0702 - Luiz Junio Veloso Dos Santos"); 
+        $display("Exemplo_0703 - Luiz Junio Veloso Dos Santos"); 
         $display("Test LU's module"); 
-        $display("  key   x    y    s"); 
+        $display("  key   x    y    s0    s1"); 
         // projetar testes do modulo 
-        $monitor("%4b %4b %4b %4b", key, x, y, s); 
+        $monitor("%4b %4b %4b %4b %4b", key, x, y, s0, s1); 
         key = 1'b0;  x = 1'b0; y = 1'b0; 
     #1  key = 1'b0;  x = 1'b0; y = 1'b1;
     #1  key = 1'b0;  x = 1'b1; y = 1'b0;
