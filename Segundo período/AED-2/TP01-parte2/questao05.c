6720efd54c50a0058efb088ddb8d21d324f29c8f
SeraphyBR <luisjuniorbr@gmail.com>
2018-10-02 08:30:27 -0300

    Update

--- Segundo período/AED-2/TP01-parte2/questao05.c
+++ Segundo período/AED-2/TP01-parte2/questao05.c
@@ -2,6 +2,7 @@
 // Autor: Luiz Junio Veloso Dos Santos
 
 #include <bsd/string.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,8 @@
 //##############################################################################
 /*
  * Descreve uma Instituicao de Ensino Superior (IES)
+ * @author Luiz Junio Veloso Dos Santos
+ * @version 1.0
  */
 typedef struct Instituicao{
     int codigo;
@@ -113,8 +116,8 @@ Instituicao* strToInstituicao(char* string)
     Instituicao* new = (Instituicao*) malloc(sizeof(Instituicao));
 
     int i = 0;
-    char* temp = strtok(string, " ");
-    char* valor[24];
+    char* temp = strtok(string, "\t");
+    char** valor = (char**) malloc(sizeof(char*) * 24);
     while(temp != NULL && i < 24){
         valor[i++] = temp;
         temp = strtok(NULL, " ");
@@ -131,20 +134,92 @@ Instituicao* strToInstituicao(char* string)
     strlcpy(new->municipio, valor[8], TAM_MUNICIPIO + 1);
     strlcpy(new->uf, valor[9], TAM_UF + 1);
     strlcpy(new->regiao, valor[10], TAM_REGIAO + 1);
-    new->tecnico = 0;
-    new->periodico = 0;
-    new->livro = 0;
-    new->receita = 0.0;
-    new->transferencia = 0.0;
-    new->outraReceita = 0.0;
-    new->despesaDocente = 0.0;
-    new->despesaTecnico = 0.0;
-    new->despesaEncargo = 0.0;
-    new->despesaCusteio = 0.0;
-    new->despesaInvestimento = 0.0;
-    new->despesaPesquisa = 0.0;
-    new->despesaOutras = 0.0;                
+    new->tecnico = atoi(valor[11]);
+    new->periodico = atoi(valor[12]);
+    new->livro = atoi(valor[13]);
+    new->receita = atof(valor[14]);
+    new->transferencia = atof(valor[15]);
+    new->outraReceita = atof(valor[16]);
+    new->despesaDocente = atof(valor[17]);
+    new->despesaTecnico = atof(valor[18]);
+    new->despesaEncargo = atof(valor[19]);
+    new->despesaCusteio = atof(valor[20]);
+    new->despesaInvestimento = atof(valor[21]);
+    new->despesaPesquisa = atof(valor[22]);
+    new->despesaOutras = atof(valor[23]);                
     return new;
 }//Fim strToInstituicao
+
+void imprime( Instituicao* i )
+{//Inicio imprime
+    printf("%d %s %s %d %s %d %d %d %s %s %s %d %d %d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+          i->codigo, i->nome, i->sigla, i->codigoMantenedora, i->mantenedora, i->categoria,
+          i->organizacao, i->codigoMunicio, i->municipio, i->uf, i->regiao, i->tecnico,
+          i->periodico, i->livro, i->receita, i->transferencia, i->outraReceita, 
+          i->despesaDocente, i->despesaTecnico, i->despesaEncargo, i->despesaCusteio,
+          i->despesaInvestimento, i->despesaPesquisa, i->despesaOutras 
+          );
+}//Fim imprime
+
+//##############################################################################
+
+/*
+ * Lista estatica de Instituicao
+ */
+typedef struct Lista{
+   int tamArray;
+   int numElementos;
+   Instituicao** array;
+} Lista;
+
+/*
+ * Cria uma nova lista de tamanho N
+ * e retorna seu endereco
+ */
+Lista* newListaN(int tamanho)
+{//Inicio newListaN
+    Lista* lista = (Lista*) malloc(sizeof(Lista));
+    lista->array = (Instituicao**) malloc(sizeof(Instituicao*) * tamanho);
+    lista->tamArray = tamanho;
+    lista->numElementos = 0;
+    return lista;
+}//Fim newListaN
+
+/*
+ * Cria uma nova lista de tamanho 5
+ */
+Lista* newLista(){
+    return newListaN(5);
+}
+
+/*
+ * Insere um elemento na primeira posicao da lista e move os demais
+ * elementos para o fim da lista.
+ */
+void inserirInicio(Lista* l, Instituicao* i)
+{//Inicio inserirInicio
+    if(l != NULL && i != NULL)
+    {//Inicio if
+        if(l->numElementos >= l->tamArray){
+            printf("Erro ao inserir!");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            for(int i = l->numElementos; i > 0; i--){
+                l->array[i] = l->array[i - 1];
+            }
+            l->array[0] = i;
+            l->numElementos++;
+        }
+    }//Fim if
+    else{
+        printf("Parametros invalidos!");
+    }
+}//Fim inserirInicio
+
+void inserirFim(Lista* l, Instituicao* i)
+{//Inicio inserirFim
+
+}//Fim inserirFim
 
 //##############################################################################
