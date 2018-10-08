2a4c50d9c1dbf6ea394c4396bf84d707c93fe312
SeraphyBR <luisjuniorbr@gmail.com>
2018-10-07 21:19:15 -0300

    Update

--- Segundo período/AC-1/Guia08/624037-LuizJunio/Exemplo_0804.v
+++ Segundo período/AC-1/Guia08/624037-LuizJunio/Exemplo_0804.v
@@ -1,18 +1,18 @@
 // ------------------------- 
-// Exemplo_0803 - Igualdade
+// Exemplo_0804 - Diferenca
 // Nome: Luiz Junio Veloso Dos Santos 
 // Matricula: 624037
 // ------------------------- 
 
-module fullIgualdade (output s, input [3:0]a, input [3:0]b);
+module fullDiferenca (output s, input [3:0]a, input [3:0]b);
 
     wire s0,s1,s2,s3;
 
-    xnor XNOR1 ( s0, a[0], b[0] );
-    xnor XNOR2 ( s1, a[1], b[1] );
-    xnor XNOR3 ( s2, a[2], b[2] );
-    xnor XNOR4 ( s3, a[3], b[3] );
-    and  AND1 ( s, s0, s1, s2, s3 );
+    xor XOR1 ( s0, a[0], b[0] );
+    xor XOR2 ( s1, a[1], b[1] );
+    xor XOR3 ( s2, a[2], b[2] );
+    xor XOR4 ( s3, a[3], b[3] );
+    or  OR1 ( s, s0, s1, s2, s3 );
 
 endmodule 
 
@@ -21,12 +21,12 @@ module test;
     reg   [3:0] x; 
     reg   [3:0] y;
     wire        s;
-    fullIgualdade FI ( s, x, y ); 
+    fullDiferenca FD ( s, x, y ); 
  
 // ------------------------- parte principal 
  initial begin 
-    $display("Exemplo0803 - Luiz Junio - 624037"); 
-    $display("Test ALU’s Igualdade");
+    $display("Exemplo0804 - Luiz Junio - 624037"); 
+    $display("Test ALU’s Diferenca");
     $display("      x        y     s");
     $monitor("  %4b      %4b     %1b", x, y, s);
         x = 4'b0100; y = 4'b0011;
