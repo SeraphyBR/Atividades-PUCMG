1dc49934d6f289df1eb5f48b914a721af46aefca
SeraphyBR <luisjuniorbr@gmail.com>
2019-01-20 19:29:42 -0200

    Update

--- Segundo período/S-2/ordenacaoParalelo/QuickSort/quickparalelo.c
+++ Segundo período/S-2/ordenacaoParalelo/QuickSort/quickparalelo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-   
+
 int arr[1000000];
 int Nthreads = 4, n = 0;
 
@@ -12,19 +12,19 @@ void swap(int* a, int* b)
     *a = *b; 
     *b = t; 
 } 
-  
+
 /*  
     Essa função usa o último elemento como pivô, 
     posiciona o elemento pivô na posição correta no 
     array ordenado e coloca todos os menores que o
     pivô à esquerda (do pivô) e todos os elementos
     maiores à direita
-*/
+    */
 int particao (int esq, int dir) 
 { 
     int pivo = arr[dir]; 
     int i = (esq - 1);  //menor elemento
-  
+
     for (int j = esq; j <= dir- 1; j++) 
     { 
         if (arr[j] <= pivo) 
@@ -44,38 +44,38 @@ void quickSort(int esq, int dir)
     if (esq < dir)
     {
         pi = particao( esq, dir);
-            
+
         if(n > 20){
-	   	    quickSort(esq, pi - 1);
+            quickSort(esq, pi - 1);
             quickSort( pi + 1, dir);
-	    }
-	    else{                           
+        }
+        else{                           
             #pragma omp parallel sections num_threads(Nthreads) 
-		    {
-		        #pragma omp section
-		        quickSort(esq, pi - 1);
-		        #pragma omp section
-		        quickSort( pi + 1, dir);
-		    }
+            {
+                #pragma omp section
+                quickSort(esq, pi - 1);
+                #pragma omp section
+                quickSort( pi + 1, dir);
+            }
         }
     }
 }
 
 void quicksort(){
-	quickSort(0, 999999);
+    quickSort(0, 999999);
 }
 
 int main() {
-   
-   int i;
-   omp_set_num_threads(4);
-   omp_set_nested(1);
 
-   for(i = 0; i < 1000000; i++){
-   	scanf ("%d", &arr[i]); 	
-   }
-   quicksort();
-   for(i = 0; i < 1000000; i++){
-   	printf ("%d\n", arr[i]);
-   }
+    int i;
+    omp_set_num_threads(4);
+    omp_set_nested(1);
+
+    for(i = 0; i < 1000000; i++){
+        scanf ("%d", &arr[i]); 	
+    }
+    quicksort();
+    for(i = 0; i < 1000000; i++){
+        printf ("%d\n", arr[i]);
+    }
 }
