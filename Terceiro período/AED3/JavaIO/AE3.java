9978f15971056ec192fda1a80badcf3d95a3c00f
SeraphyBR <luisjuniorbr@gmail.com>
2019-02-20 18:27:25 -0300

    Update

---
+++ Terceiro período/AED3/JavaIO/AE3.java
@@ -0,0 +1,20 @@
+/*
+ * AE3.java
+ * Copyright (C) 2019 seraphybr-fun <seraphybr-fun@funtoo-z40>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+package aed3;
+import java.io.*;
+
+public class AE3
+{
+    public static void main(String[] args) {
+        FileWriter arq;
+        try {
+            arq = FileWriter("dados.txt", false);
+        }
+    }
+}
+
