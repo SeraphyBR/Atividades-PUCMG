1a5f057a997fffee4d5572dcc439c91d89705062
SeraphyBR <luisjuniorbr@gmail.com>
2019-03-14 07:03:13 -0300

    Add relatorio05

--- Terceiro período/LAC/Relatorio05/relatorio05-parte2.ino
+++ Terceiro período/LAC/Relatorio05/relatorio05-parte2.ino
@@ -1,6 +1,7 @@
 // Definiçao de valores para variáveis  
 int led10 = 10, led11 = 11, led12 = 12, led13 = 13;
 int a = 0, b = 0, op = 0, result = 0, vaiUm = 0;
+String entrada = " ";
 // put your setup code here, to run once:
 void setup() {
     Serial.begin(9600);
@@ -15,9 +16,10 @@ void loop() {
     // put your main code here, to run repeatedly:
 
     if(Serial.available() > 0){
-        a = Serial.parseInt();
-        b = Serial.parseInt();
-        op = Serial.parseInt();	
+        entrada = Serial.readString();
+        a = charToInt(entrada.charAt(0));
+        b = charToInt(entrada.charAt(1));
+        op = charToInt(entrada.charAt(2));
     }
     mostraBin(led13, a);
     mostraBin(led12, b);
@@ -36,11 +38,18 @@ void loop() {
             vaiUm = a & b;
             break;
         default:
+            Serial.println("Operacao invalida!");
             break;
     }	
     mostraBin(led11, result);
     mostraBin(led10, vaiUm);
 }        
+
+int charToInt(char c){
+    String s = String(c);
+    int i = s.toInt();
+    return i;
+}
 
 void mostraBin(int led, int a){
     if(a != 0) digitalWrite(led,HIGH);
