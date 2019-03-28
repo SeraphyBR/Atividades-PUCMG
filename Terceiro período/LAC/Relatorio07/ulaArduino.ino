ceb96ee50a6f7e9bcf798a49e568d53c842930d6
SeraphyBR <luisjuniorbr@gmail.com>
2019-03-27 23:02:37 -0300

    Update

--- Terceiro período/LAC/Relatorio07/ulaArduino.ino
+++ Terceiro período/LAC/Relatorio07/ulaArduino.ino
@@ -1,7 +1,6 @@
 // Definiçao de valores para variáveis  
 int led10 = 10, led11 = 11, led12 = 12, led13 = 13;
-int a = 0, b = 0, op = 0, result = 0, vaiUm = 0;
-String entrada = " ";
+
 // put your setup code here, to run once:
 void setup() {
     Serial.begin(9600);
@@ -12,41 +11,61 @@ void setup() {
     pinMode(led13, OUTPUT);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-
-    if(Serial.available() > 0){
-        entrada = Serial.readString();
-        a = charToInt(entrada.charAt(0));
-        b = charToInt(entrada.charAt(1));
-        op = charToInt(entrada.charAt(2));
-    }
-    mostraBin(led13, a);
-    mostraBin(led12, b);
-    switch(op){
-        case 0:
-            result = a & b;
-            vaiUm = 0;
+String hexToBin(char h)
+{//Begin hexToBin
+    String bin = ""; 
+    switch(h){
+        case '0':
+            bin = "0000";
             break;
-        case 1:
-            result = a | b;
-            vaiUm = 0;
+        case '1':
+            bin = "0001";
             break;
-        case 2:
-            result = ~a;
-            vaiUm = 0;
+        case '2':
+            bin = "0010";
             break;
-        case 3:
-            result = a ^ b;
-            vaiUm = a & b;
+        case '3':
+            bin = "0011";
             break;
-        default:
-            Serial.println("Operacao invalida!");
+        case '4':
+            bin = "0100";
             break;
-    }	
-    mostraBin(led11, result);
-    mostraBin(led10, vaiUm);
-}        
+        case '5':
+            bin = "0101";
+            break;
+        case '6':
+            bin = "0110";
+            break;
+        case '7':
+            bin = "0111";
+            break;
+        case '8':
+            bin = "1000";
+            break;
+        case '9':
+            bin = "1001";
+            break;
+        case 'A':
+            bin = "1010";
+            break;
+        case 'B':
+            bin = "1011";
+            break;
+        case 'C':
+            bin = "1100";
+            break;
+        case 'D':
+            bin = "1101";
+            break;
+        case 'E':
+            bin = "1110";
+            break;
+        case 'F':
+            bin = "1111";
+            break;  
+    } 
+    return bin;
+}//End hexToBin
 
 int charToInt(char c){
     String s = String(c);
@@ -54,7 +73,137 @@ int charToInt(char c){
     return i;
 }
 
-void mostraBin(int led, int a){
-    if(a != 0) digitalWrite(led,HIGH);
+void mostraBin(int led, char a){
+    if(a != '0') digitalWrite(led,HIGH);
     else digitalWrite(led,LOW);
-}  
+} 
+
+String Neg(String binary){
+    String result = "";
+    for(int c = 0; c < binary.length(); c++){
+        if(binary.charAt(c) == '0') result += "1";
+        else result += "0";
+    }
+    return result;
+}
+
+String Or(String a, String b){
+    String result = "";
+    if(a.length() == b.length()){
+        for(int c = 0; c < a.length(); c++){
+            if(a.charAt(c) != b.charAt(c)) result += "1";
+            else result += a.charAt(c);
+        }
+    }
+    return result;
+}
+
+String And(String a, String b){
+    String result = "";
+    if(a.length() == b.length()){
+        for(int c = 0; c < a.length(); c++){
+            if(a.charAt(c) == b.charAt(c)) result += a.charAt(c);
+            else result += "0";
+        }
+    }
+    return result;
+}
+
+String Xor(String a, String b){
+    String result = "";
+    if(a.length() == b.length()){
+        for(int c = 0; c < a.length(); c++){
+            if(a.charAt(c) != b.charAt(c)) result += "1";
+            else result += "0";
+        }
+    }
+    return result;
+}
+
+String instruction(String a, String b, char select)
+{//Begin instruction
+    String result = "";
+    switch(select){
+        case '0':
+            result = "0000";
+            break;
+        case '1':
+            result = "1111";
+            break;
+        case '2':
+            result = Neg(a);
+            break;
+        case '3':
+            result = Neg(b);
+            break;
+        case '4':
+            result = Or(a, b);
+            break;
+        case '5':
+            result = And(a, b);
+            break;
+        case '6':
+            result = Xor(a, b);
+            break;
+        case '7':           
+            result = Neg(And(a, b));
+            break;
+        case '8':
+            result = Neg(Or(a, b));
+            break;
+        case '9':
+            result = Neg(Xor(a, b));
+            break;
+        case 'A':
+            result = Or(Neg(a), b);
+            break;
+        case 'B':
+            result = Or(a, Neg(b));
+            break;
+        case 'C':
+            result = And(Neg(a), b);
+            break;
+        case 'D':
+            result = And(a, Neg(b));
+            break;
+        case 'E':
+            result = Or(Neg(a), Neg(b));
+            break;
+        case 'F':
+            result = And(Neg(a), Neg(b));
+            break;
+        default:
+            Serial.println("Operacao invalida!");
+            break;
+    }
+    return result;
+}//End instruction
+
+void loop()
+{//Begin main loop
+    // put your main code here, to run repeatedly:
+    if(Serial.available() > 0){
+        String entrada = Serial.readString();
+        char a = entrada.charAt(0);
+        char b = entrada.charAt(1);
+        char select = entrada.charAt(2);
+        if(Serial.read() == '\n'){
+            String result = instruction(hexToBin(a), hexToBin(b), select);
+            if(result.length() == 4){
+                mostraBin(led10, result.charAt(3));
+                mostraBin(led11, result.charAt(2));
+                mostraBin(led12, result.charAt(1));
+                mostraBin(led13, result.charAt(0));
+                Serial.println(result);  
+            }
+            else {
+                mostraBin(led10, 0);
+                mostraBin(led11, 0);
+                mostraBin(led12, 0);
+                mostraBin(led13, 0);
+                Serial.println(result); 
+            }
+        }
+    }
+}//End main loop    
+
