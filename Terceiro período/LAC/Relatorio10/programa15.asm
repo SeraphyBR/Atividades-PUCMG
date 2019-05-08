7a8f044d864a0ba9c1837a9ecd154f8e64df8dbc
SeraphyBR <luisjuniorbr@gmail.com>
2019-05-08 17:35:21 -0300

    Update

--- Terceiro período/LAC/Relatorio10/programa15.asm
+++ Terceiro período/LAC/Relatorio10/programa15.asm
@@ -1,22 +1,16 @@
-
 # i - $s0 
-
+# soma - $s1
 .text
 .globl main
-
 main:
-
 	# Colocar endereco base da memoria em t0
 	addi $t0,$t0,0x1001
 	sll  $t0,$t0,16
 	
-	# Comeca i = 0
-	addi $s0,$zero,0
+	addi $s0,$zero,0 # comeca i = 0
+	addi $t3,$zero,100 # Colocando t3 = 100
 	
-	# Colocando t3 = 100
-	addi $t3,$zero,100
-	
-do:
+do: # Preenche o vetor de 100 
 	# t1 = 2 * i + 1
 	sll $t1,$s0,1
 	addi $t1,$t1,1
@@ -25,14 +19,29 @@ do:
 	sll $t2,$s0,2
 	add $t2,$t2,$t0
 	sw $t1,0($t2)
-	
-	# i++
-	addi $s0,$s0,1
+
+	addi $s0,$s0,1 # i++
 	
 	# i != t3
 	bne $s0,$t3,do
-	j exit
+	j endWhile
 	
-exit:
+endWhile:
+	addi $s0,$zero,0 # Define i = 0
+	addi $s1,$zero,0 # Define soma = 0
 
+do2:
+	# soma += vetor[i]
+	sll $t2,$s0,2
+	add $t2,$t2,$t0
+	lw $t4, 0($t2)
+	add $s1,$s1,$t4
 	
+	addi $s0,$s0,1 # i++
+	
+	# i != t3
+	bne $s0,$t3,do2
+	j endWhile2
+	
+endWhile2:
+	sw $s1, 0($t2)
