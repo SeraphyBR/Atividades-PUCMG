e079f4b2a2759740ed8168dde0cc3b9201d5cb55
SeraphyBR <luisjuniorbr@gmail.com>
2019-06-12 08:50:04 -0300

    update

--- Primeiro período/AED-1/Java/Trabalho Prático 01 - AED1/SegundoGrau.java
+++ Primeiro período/AED-1/Java/Trabalho Prático 01 - AED1/SegundoGrau.java
@@ -1,18 +1,18 @@
 // Autor: Luiz Junio <luisjuniorbr@gmail.com>
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class SegundoGrau    
+public class SegundoGrau
 {
-    public static void main(String[] args) 
-    {   
+    public static void main(String[] args)
+    {
         Scanner leia = new Scanner(System.in);
         System.out.println("Uma equação de segundo grau obedece o seguinte formato: Ax²+Bx+C");
         System.out.print("Digite o valor de A: ");
         double a = leia.nextDouble();
         System.out.print("Digite o valor de B: ");
         double b = leia.nextDouble();
-        System.out.print("Digite o valor de C: "); 
+        System.out.print("Digite o valor de C: ");
         double c = leia.nextDouble();
 
         if (a == 0){
@@ -20,7 +20,7 @@ public class SegundoGrau
             a = leia.nextDouble();
         }
 
-        double x1 = ((0 - (b)) + Math.sqrt(Math.pow(b,2) - 4*a*c)) / (2.0 * a) ; 
+        double x1 = ((0 - (b)) + Math.sqrt(Math.pow(b,2) - 4*a*c)) / (2.0 * a) ;
         double x2 = ((0 - (b)) - Math.sqrt(Math.pow(b,2) - 4*a*c)) / (2.0 * a);
 
         if ( x1 != x1 ){
@@ -32,5 +32,5 @@ public class SegundoGrau
         else
             System.out.print("As raízes são: " + x1 + " e " + x2);
 
-    } 
-} 
+    }
+}
--- Primeiro período/AED-1/Rust/Trabalho Prático 01 - AED1/segundo_grau/src/main.rs
+++ Primeiro período/AED-1/Rust/Trabalho Prático 01 - AED1/segundo_grau/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use std::io::Write;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Uma equação de segundo grau obedece o seguinte formato: Ax²+Bx+C");
+
+    print!("Digite o valor de A: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let a = input.trim().parse::<f32>().unwrap();
+    input.clear();
+
+    print!("Digite o valor de B: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let b = input.trim().parse::<f32>().unwrap();
+    input.clear();
+
+    print!("Digite o valor de C: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let c = input.trim().parse::<f32>().unwrap();
+    input.clear();
+
+    let delta = b.powi(2) - 4.0 * a * c;
+    let x1 = (- b + delta.sqrt()) / (2.0 * a);
+    let x2 = (- b - delta.sqrt()) / (2.0 * a);
+
+    match delta.partial_cmp(&0.0).unwrap() {
+        Ordering::Less => println!("Não há solução para essa equação!"),
+        Ordering::Equal => println!("A raiz é: {}", x1),
+        Ordering::Greater => println!("As raízes são: {} e {}", x1, x2),
+    };
+
 }
